3140d4c801e64a9608c6855bfa5167c429eebe57
Alejandra Landa <aleriquene@gmail.com>
2022-02-24 17:30:44 -0600

    commit tree 338527c3171223656f696ed29034bddfa67af7f4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function  superbowlWin(record){
+    const win=record.find(record=> record.result="2015")
+}
+
+
+
+const won = year.find(year)
+    console.log(won)
+function yearWon(ano){
+    return year.won ==='2015'
+}
